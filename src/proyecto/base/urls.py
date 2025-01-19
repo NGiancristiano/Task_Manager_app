0ad19fa4d3e2ffa2112a30774404324d303daf9d
Nicolas Giancristiano <ngiancristiano1@gmail.com>
2025-01-19 23:27:31 +0100

    adding styles and correcting some views

--- src/proyecto/base/urls.py
+++ src/proyecto/base/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ListaPendientes,DetalleTarea,CrearTarea,EditarTarea, EliminarTarea, Logueo
+from .views import ListaPendientes,DetalleTarea,CrearTarea,EditarTarea, EliminarTarea, Logueo, Registro
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [path('',ListaPendientes.as_view(), name='tareas'),
@@ -8,4 +8,5 @@ urlpatterns = [path('',ListaPendientes.as_view(), name='tareas'),
                path('editar-tarea/<int:pk>',EditarTarea.as_view(), name='editar-tarea'),
                path('eliminar-tarea/<int:pk>',EliminarTarea.as_view(), name='eliminar-tarea'),
                path('login/', Logueo.as_view(), name = 'login'),
-               path('logout/', LogoutView.as_view(), name = 'logout')]
+               path('logout/', LogoutView.as_view(), name = 'logout'),
+               path('registro/', Registro.as_view(), name = 'registro')]
--- src/proyecto/base/views.py
+++ src/proyecto/base/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
@@ -10,6 +12,17 @@ from .models import Tarea
 class ListaPendientes(LoginRequiredMixin, ListView):
     model = Tarea
     context_object_name = 'tareas'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tareas'] = context['tareas'].filter(usuario = self.request.user)
+        context['count'] = context['tareas'].filter(completo=False).count()
+
+        valor_buscado = self.request.GET.get('area-buscar') or ''
+        if valor_buscado:
+            context['tareas'] = context['tareas'].filter(titulo__icontains = valor_buscado)
+        context['valor_buscado'] = valor_buscado
+        return context
 
 
 class Logueo(LoginView):
@@ -20,6 +33,23 @@ class Logueo(LoginView):
     def get_success_url(self):
         return reverse_lazy('tareas')
 
+class Registro(FormView):
+    template_name = 'base/registro.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tareas')
+
+    def form_valid(self, form):
+        usuario = form.save()
+        if usuario is not None:
+            login(self.request,usuario)
+        return super(Registro, self).form_valid(form)
+
+    def get(self,*args,**kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tareas')
+        return super(Registro,self).get(*args,**kwargs)
+
 
 class DetalleTarea(LoginRequiredMixin, DetailView):
     model = Tarea
@@ -28,13 +58,17 @@ class DetalleTarea(LoginRequiredMixin, DetailView):
 
 class CrearTarea(LoginRequiredMixin, CreateView):
     model = Tarea
-    fields = '__all__'
+    fields = ['titulo','descripcion','completo']
     success_url = reverse_lazy('tareas')
+
+    def form_valid(self, form):
+        form.instance.usuario = self.request.user
+        return super(CrearTarea, self).form_valid(form)
 
 
 class EditarTarea(LoginRequiredMixin, UpdateView):
     model = Tarea
-    fields = '__all__'
+    fields = ['titulo', 'descripcion', 'completo']
     success_url = reverse_lazy('tareas')
 
 
