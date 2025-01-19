df057f4eaf8880f76cb6ed3b94933e2001e6908b
Nicolas Giancristiano <ngiancristiano1@gmail.com>
2025-01-19 14:31:13 +0100

    adding more features

--- src/proyecto/base/views.py
+++ src/proyecto/base/views.py
@@ -1,19 +1,44 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from .models import Tarea
 
-class ListaPendientes(ListView):
+class ListaPendientes(LoginRequiredMixin, ListView):
     model = Tarea
     context_object_name = 'tareas'
 
-class DetalleTarea(DetailView):
+
+class Logueo(LoginView):
+    template_name = 'base/login.html'
+    field = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tareas')
+
+
+class DetalleTarea(LoginRequiredMixin, DetailView):
     model = Tarea
     context_object_name = 'tarea'
 
-class CrearTarea(CreateView):
+
+class CrearTarea(LoginRequiredMixin, CreateView):
     model = Tarea
     fields = '__all__'
+    success_url = reverse_lazy('tareas')
+
+
+class EditarTarea(LoginRequiredMixin, UpdateView):
+    model = Tarea
+    fields = '__all__'
+    success_url = reverse_lazy('tareas')
+
+
+class EliminarTarea(LoginRequiredMixin, DeleteView):
+    model = Tarea
+    context_object_name = 'tarea'
     success_url = reverse_lazy('tareas')
--- src/proyecto/proyecto/settings.py
+++ src/proyecto/proyecto/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+from django.conf.global_settings import LOGIN_URL
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -111,6 +113,10 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+LOGOUT_REDIRECT_URL = 'login'
+
+LOGIN_URL = 'login'
 
 
 # Static files (CSS, JavaScript, Images)
